f9c457058cebf10a5c03f3375e4de7fe0fd59e53
pcatapan <pacho30000@gmail.com>
2022-06-15 12:42:53 +0200

    Add new funcion

--- test.c
+++ test.c
@@ -6,23 +6,29 @@
 /*   By: pcatapan <pcatapan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 19:04:57 by pcatapan          #+#    #+#             */
-/*   Updated: 2022/06/14 22:18:54 by pcatapan         ###   ########.fr       */
+/*   Updated: 2022/06/15 12:40:13 by pcatapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <termios.h>
+#include <term.h>
 #include <unistd.h>
-#include <fcntl.h>
-
+#include <termcap.h>
+#include <stdlib.h>
+#include <curses.h>
+int putchar(int tc)
+{
+	write(1, &tc, 1);
+	return (0);
+}
+ 
 int	main(void)
 {
-	int		fd;
-	char	*d;
+	char *env = getenv("TERM");
+	if (env == NULL)
+		env = "xterm";
 
-	fd = ttyslot();
-	if (isatty(fd))
-		printf("aaa\n");
-	d = ttyname(fd);
-	printf("d - %s\n", d);
+	tgetent(NULL, env); 
+	char *cm = tgetstr("cm", NULL);
+
+	tputs(tgoto(cm, 5, 5), 1, putchar);
 }
