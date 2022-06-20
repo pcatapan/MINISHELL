67f89193c4d95919b831d24e230c305515aa4d87
iri-angh <irina.anghel42@gmail.com>
2022-06-20 10:26:00 +0200

    mini struttura nel .h, aggiunte piccole info sui comandi

--- minishell.h
+++ minishell.h
@@ -3,11 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcatapan <pcatapan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aanghel <aanghel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:59:21 by pcatapan          #+#    #+#             */
-/*   Updated: 2022/06/14 18:59:38 by pcatapan         ###   ########.fr       */
+/*   Updated: 2022/06/20 09:33:27 by aanghel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/stat.h>
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <sys/stat.h>
+
+typedef struct s_data
+{
+	int		input; //da valutare sia il tipo e sia se serve veramente
+	int		output; //idem di input
+	int		pipe;
+	char	token;
+	char	**matrix;
+}	t_data;
+
+#endif
