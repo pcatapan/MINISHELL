5f64b1f6d5389d627e777bd73ea6655dbe5f40e8
pcatapan <pacho30000@gmail.com>
2022-11-20 19:15:21 +0100

    Fix the echo "ab\""

--- src/built_in/echo.c
+++ src/built_in/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aanghel <aanghel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pcatapan <pcatapan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 19:10:04 by pcatapan          #+#    #+#             */
-/*   Updated: 2022/11/20 18:37:57 by aanghel          ###   ########.fr       */
+/*   Updated: 2022/11/20 19:13:47 by pcatapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ void	ft_echo(t_token *token)
 		i++;
 	while (token->value[i])
 	{
-		token->value[i] = ft_strtrim2(token->value[i], '"');
-		token->value[i] = ft_strtrim2(token->value[i], '\'');
 		if (ft_strcmp(token->value[1], "$?"))
 		{
 			printf("%d\n", g_exit);
@@ -65,8 +63,38 @@ void	ft_echo_and(t_token *token)
 	ft_echo(token);
 }
 
+void	ft_preparet_echo(t_token *token)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = -1;
+	while (token->value[++i])
+	{
+		j = 0;
+		while (token->value[i][j])
+		{
+			if (token->value[i][j] == '"' || token->value[i][j] == '\'')
+			{
+				if (token->value[i][j] == '"' && \
+					token->value[i][j - 1] == '\\')
+					token->value[i][j - 1] = 127;
+				else
+					token->value[i][j] = 127;
+			}
+			j++;
+		}
+		tmp = ft_strtrim2(token->value[i], 127);
+		// free(token->value[i]);
+		token->value[i] = ft_strdup(tmp);
+		free(tmp);
+	}
+}
+
 void	ft_check_echo(t_token *token)
 {
+	ft_preparet_echo(token);
 	if (token->prev)
 	{
 		if (token->prev->or)
